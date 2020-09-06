c2a8ec7bfd388d5948c0aa4b1d768115fe18550b
Nathan Graule <solarliner@gmail.com>
2020-09-06 21:18:28 +0200

    fix: use orderable for blog author model, and clean up blog template

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,26 @@
 from django.db import models
+from modelcluster.fields import ParentalKey
 
-from wagtail.core.models import Page
-from wagtail.core import blocks
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
 from wagtail.core.blocks import RichTextBlock
 from wagtail.core.fields import StreamField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.admin.edit_handlers import StreamFieldPanel
-from wagtail_pygments.blocks import CodeBlock
+from wagtail_pygments import blocks as pygments_blocks
 from wagtail.search import index
+
+
+class CodeBlock(pygments_blocks.CodeBlock):
+    class Meta:
+        icon = "code"
+        value_class = pygments_blocks.CodeStructValue
+        template = "blocks/code_block.html"
 
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
+    parent_page_types = ['home.HomePage']
     subpage_types = ['blog.BlogPage']
 
     content_panels = Page.content_panels + [
@@ -26,20 +34,20 @@ class BlogIndexPage(Page):
         return context
 
 
-class AuthorBlock(blocks.StructBlock):
-    class Meta:
-        icon = "user"
-    name = blocks.CharBlock(required=False, default="Unknown", max_length=50)
-    email = blocks.EmailBlock(required=False)
+class BlogAuthor(Orderable):
+    page = ParentalKey("blog.BlogPage", related_name="authors")
+    name = models.CharField(default="Anonymous", max_length=50)
+    email = models.EmailField(null=True, blank=True)
+
+    panels = [
+        FieldPanel("name"),
+        FieldPanel("email")
+    ]
 
 
 class BlogPage(Page):
     date = models.DateField('Post date')
     intro = models.CharField(max_length=250)
-
-    authors = StreamField([
-        ('author', AuthorBlock())
-    ], null=True)
 
     content = StreamField([
         ('rich_text', RichTextBlock()),
@@ -53,7 +61,7 @@ class BlogPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
-        StreamFieldPanel('authors'),
+        MultiFieldPanel([InlinePanel("authors", min_num=1)], heading="Auteurs"),
         FieldPanel('intro'),
         StreamFieldPanel('content')
     ]
