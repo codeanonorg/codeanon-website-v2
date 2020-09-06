e01f7a482328b142788e66d12a393142c26fa187
Arthur Correnson <arthur.correnson@gmail.com>
2020-09-06 19:18:11 +0200

    Fix the blog template

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core import blocks
 from wagtail.core.fields import RichTextField
 from wagtail.core.blocks import RichTextBlock
 from wagtail.core.fields import StreamField
@@ -12,27 +13,47 @@ from wagtail.search import index
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
+    subpage_types = ['blog.BlogPage']
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname='full')
     ]
 
+    def get_context(self, request):
+        context = super().get_context(request)
+        articles = self.get_children().live().order_by('-first_published_at')
+        context['articles'] = articles
+        return context
+
+
+class AuthorBlock(blocks.StructBlock):
+    class Meta:
+        icon = "user"
+    name = blocks.CharBlock(required=False, default="Unknown", max_length=50)
+    email = blocks.EmailBlock(required=False)
+
 
 class BlogPage(Page):
     date = models.DateField('Post date')
     intro = models.CharField(max_length=250)
-    body = StreamField([
+
+    authors = StreamField([
+        ('author', AuthorBlock())
+    ], null=True)
+
+    content = StreamField([
         ('rich_text', RichTextBlock()),
         ('code', CodeBlock())
     ])
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
-        index.SearchField('body')
+        index.SearchField('content')
     ]
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
+        StreamFieldPanel('authors'),
         FieldPanel('intro'),
-        StreamFieldPanel('body')
+        StreamFieldPanel('content')
     ]
